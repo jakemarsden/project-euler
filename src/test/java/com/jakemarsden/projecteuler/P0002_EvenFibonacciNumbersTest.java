5e66a770469c581d132593a738139d473c6cd7a0
Jake Marsden <jakemarsdenjm@gmail.com>
2020-02-08 15:15:25 +1300

    Problem 2: Even Fibonacci numbers
    Solution is 4,613,732

---
+++ src/test/java/com/jakemarsden/projecteuler/P0002_EvenFibonacciNumbersTest.java
@@ -0,0 +1,20 @@
+package com.jakemarsden.projecteuler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class P0002_EvenFibonacciNumbersTest {
+
+  @Test
+  void givenSolution() {
+    var obj = new P0002_EvenFibonacciNumbers();
+    assertEquals(44, obj.sum(89));
+  }
+
+  @Test
+  void targetSolution() {
+    var obj = new P0002_EvenFibonacciNumbers();
+    assertEquals(4_613_732, obj.sum(4_000_000));
+  }
+}
