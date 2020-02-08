ec274cd6e886dd1d07e5baa5a227d3a7b350254f
Jake Marsden <jakemarsdenjm@gmail.com>
2020-02-08 16:44:28 +1300

    Problem 4: Largest palindrome product
    Solution is 906,609

---
+++ src/test/java/com/jakemarsden/projecteuler/P0004_LargestPalindromeProductTest.java
@@ -0,0 +1,20 @@
+package com.jakemarsden.projecteuler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class P0004_LargestPalindromeProductTest {
+
+  @Test
+  void givenSolution() {
+    var obj = new P0004_LargestPalindromeProduct();
+    assertEquals(90_09, obj.product(2));
+  }
+
+  @Test
+  void targetSolution() {
+    var obj = new P0004_LargestPalindromeProduct();
+    assertEquals(906_609, obj.product(3));
+  }
+}
