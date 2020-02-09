6ff1de6c1461e03cbf927328e69023d4d2011782
Jake Marsden <jakemarsdenjm@gmail.com>
2020-02-09 15:05:45 +1300

    Problem 12: Highly divisible triangular number
    Solution is 76,576,500

---
+++ src/test/java/com/jakemarsden/projecteuler/P0012_HighlyDivisibleTriangularNumberTest.java
@@ -0,0 +1,20 @@
+package com.jakemarsden.projecteuler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class P0012_HighlyDivisibleTriangularNumberTest {
+
+  @Test
+  void givenSolution() {
+    var obj = new P0012_HighlyDivisibleTriangularNumber();
+    assertEquals(28, obj.firstWithOverNDivisors(5));
+  }
+
+  @Test
+  void targetSolution() {
+    var obj = new P0012_HighlyDivisibleTriangularNumber();
+    assertEquals(76_576_500, obj.firstWithOverNDivisors(500));
+  }
+}
