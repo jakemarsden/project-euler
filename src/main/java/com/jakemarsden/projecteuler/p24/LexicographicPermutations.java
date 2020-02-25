43e976792b98087e3fd2fa1f8599418e88919ea8
Jake Marsden <jakemarsdenjm@gmail.com>
2020-02-26 02:44:57 +1300

    Optimise Problem 24
    Reduce runtime from ~1.5 seconds to ~5 ms.
    
    Work out the value of each digit in order, without actually generating
    all of the previous sequences.
    
    - Problem 24: Lexicographic permutations

--- src/main/java/com/jakemarsden/projecteuler/p24/LexicographicPermutations.java
+++ src/main/java/com/jakemarsden/projecteuler/p24/LexicographicPermutations.java
@@ -1,18 +1,13 @@
 package com.jakemarsden.projecteuler.p24;
 
-import static java.lang.System.arraycopy;
+import static com.jakemarsden.projecteuler.util.MathUtils.factorial;
+import static java.util.stream.Collectors.toList;
 
-import java.util.function.Function;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 final class LexicographicPermutations {
 
   /**
-   * TODO: Improve efficiency => We don't need to actually generate each permutation as it should be
-   * possible to <em>do the maths</em> to "jump" straight to the <code>n<sup>th</sup></code>
-   * permutation
-   *
    * @param n One-based
    * @param digitCount Digits used will be in the half-open range {@code [0, digitCount)}
    * @return An integer representing the <code>n<sup>th</sup></code> lexicographic permutation of
@@ -22,62 +17,22 @@ final class LexicographicPermutations {
     assert n > 0;
     assert digitCount > 0;
 
-    var digits = new int[digitCount];
-    for (int digit = 0; digit < digitCount; digit++) digits[digit] = digit;
+    // make `n` zero-based
+    n--;
 
-    return permutationsOf(digits)
-        .skip(n - 1)
-        .findFirst()
-        .map(this::permutationAsInt)
-        .orElseThrow(); //
-  }
+    // ordered list of digits not yet used by the permutation
+    var remainingDigits = IntStream.range(0, digitCount).boxed().collect(toList());
+    var permutation = new int[digitCount];
 
-  /**
-   * Returns a stream of all possible permutations of the given {@code items}, in order.
-   *
-   * <p>Example usage ({@code n=3}):
-   *
-   * <pre><code>
-   * permutationsOf(0, 1, 2) == Stream.of(
-   *   {0, 1, 2},
-   *   {0, 2, 1},
-   *   {1, 0, 2},
-   *   {1, 2, 0},
-   *   {2, 0, 1},
-   *   {2, 1, 0}
-   * )</code></pre>
-   */
-  private Stream<int[]> permutationsOf(int... items) {
-    if (items.length == 1)
-      // Base case: there is 1 permutation of 1 item
-      return Stream.of(items);
+    for (int digitIdx = 0; digitIdx < digitCount; digitIdx++) {
+      var digit = (int) (n / factorial(digitCount - digitIdx - 1));
+      digit %= remainingDigits.size();
 
-    // General case: there are `n!` permutations of `n` items
-    return IntStream.range(0, items.length)
-        .mapToObj(
-            prefixIdx -> {
-              // Each item has a "go" at being the *prefix*...
-              var prefixItem = items[prefixIdx];
+      // move target digit from the list of remaining digits onto the end of the permutation
+      permutation[digitIdx] = remainingDigits.remove(digit);
+    }
 
-              // ...leaving each of the remaining items as part of the *suffix*...
-              var suffixItems = new int[items.length - 1];
-              for (int itemIdx = 0, suffixIdx = 0; itemIdx < items.length; itemIdx++) {
-                if (itemIdx != prefixIdx) suffixItems[suffixIdx++] = items[itemIdx];
-              }
-
-              // ...so all *suffix permutations* are found recursively...
-              var suffixPermutations = this.permutationsOf(suffixItems);
-
-              // ...leading to one permutation for each *prefix*/*suffix permutation* pair
-              return suffixPermutations.map(
-                  suffixPermutation -> {
-                    var permutation = new int[items.length];
-                    permutation[0] = prefixItem;
-                    arraycopy(suffixPermutation, 0, permutation, 1, suffixPermutation.length);
-                    return permutation;
-                  });
-            })
-        .flatMap(Function.identity());
+    return permutationAsInt(permutation);
   }
 
   private long permutationAsInt(int[] permutation) {
